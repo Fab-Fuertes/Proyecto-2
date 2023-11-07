19ca96fbf593a723df8707f0d0d5a550aa180a27
Fab-Fuertes <fabiana.fuertes@correo.unimet.edu.ve>
2023-11-06 21:02:43 -0400

    Creacion de la clase usuario

---
+++ src/proyecto/Estructuras/Usuario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.Estructuras;
+
+/**
+ *
+ * @author fabys
+ */
+public class Usuario {
+    private String usuario;
+    private String prioridad;
+    
+    public Usuario(){
+        this.usuario = null;
+        this.prioridad = null;
+    }
+}
