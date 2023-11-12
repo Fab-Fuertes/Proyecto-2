d56967d663d9e54b81ca4a910de065854cef3031
Truenno547YT <d.andres@correo.unimet.edu.ve>
2023-11-12 18:45:23 -0400

    implementacion parcial del metodo insertar en el monticulo binario

---
+++ src/proyecto/Estructuras/Nodo_Arbol.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.Estructuras;
+
+/**
+ *
+ * @author truenno
+ */
+public class Nodo_Arbol<T> {
+	private T data;
+	private int priority;
+	private Nodo_Arbol pLeft;
+	private Nodo_Arbol pRight;
+
+	public Nodo_Arbol(T data, int priority) {
+		this.data = data;
+		this.pLeft = null;
+		this.pRight = null;
+		this.priority = priority;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public Nodo_Arbol getpLeft() {
+		return pLeft;
+	}
+
+	public void setpLeft(Nodo_Arbol pLeft) {
+		this.pLeft = pLeft;
+	}
+
+	public Nodo_Arbol getpRight() {
+		return pRight;
+	}
+
+	public void setpRight(Nodo_Arbol pRight) {
+		this.pRight = pRight;
+	}
+	
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+}
